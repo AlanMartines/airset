ca10e2c432bf6c885383279b997c8a5d863e1938
Alan Martines <alancpmartines@hotmail.com>
2022-03-19 20:10:07 -0400

    Create airset_install.sh

---
+++ airset_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Instalação completa do Airset
+echo "Instalação completa do Airset"
+echo ""
+sudo apt-get upgrade -y && sudo apt-get update -y
+sudo apt-get install aircrack-ng -y
+sudo apt-get install aireplay-ng -y
+sudo apt-get install airmon-ng -y
+sudo apt-get install airodump-ng -y
+sudo apt-get install awk -y
+sudo apt-get install bully -y
+sudo apt-get install curl -y
+sudo apt-get install isc-dhcp-server -y
+sudo apt-get install hostapd -y
+sudo apt-get install iwconfig -y
+sudo apt-get install lighttpd -y
+sudo apt-get install macchanger -y
+sudo apt-get install mdk3 -y
+sudo apt-get install nmap -y
+sudo apt-get install php-cgi -y
+sudo apt-get install pyrit -y
+sudo apt-get install python -y
+sudo apt-get install reaver -y
+sudo apt-get install unzip -y
+sudo apt-get install xterm -y
+sudo apt-get install zenity -y
+sudo apt-get install openssl -y
+sudo apt-get install rfkill -y
+sudo apt-get install strings -y
+sudo apt-get install fuser -y
+sudo apt-get install binutils -y
+echo ""
+sudo ./airset
+echo ""
